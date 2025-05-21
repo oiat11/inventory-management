3b49ecfe445675f8c2d40b2e2371db63dc709b0d
oiat11 <fu.qia@northeastern.edu>
2025-05-21 10:06:42 -0700

    add api query of dashboard to the front end

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,10 +1,62 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
+export interface Product{
+    productId: string;
+    name: string;
+    price: number;
+    rating?: number;
+    stockQuantity?: number;
+}
+
+export interface SaleSummary {
+    saleSummaryId: string;
+    totalSale: number;
+    changePercentage: number;
+    date: String
+}
+
+export interface PurchaseSummary {
+    purchaseSummaryId: string;
+    totalPurchased: number;
+    changePercentage: number;
+    date: String
+}
+
+export interface ExpenseSummary {
+    expenseSummaryId: string;
+    totalExpense: number;
+    changePercentage: number;
+    date: String
+}
+
+export interface ExpenseByCategorySummary {
+    expenseByCategorySummaryId: string;
+    category: string;
+    amount: number;
+    date: String
+}
+
+export interface DashboardMetrics {
+    popularProducts: Product[];
+    saleSummary: SaleSummary[];
+    purchaseSummary: PurchaseSummary[];
+    expenseSummary: ExpenseSummary[];
+    expenseByCategorySummary: ExpenseByCategorySummary[];
+}
+
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl:process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: 'api',
-    tagTypes: [],
-    endpoints:(build) => ({}),
+    tagTypes: ['DashboardMetrics'],
+    endpoints:(build) => ({
+        getDashboardMetrics: build.query<DashboardMetrics, void>({
+            query: () => ({
+                url: '/dashboard',
+                method: 'GET',
+            }),
+            providesTags: ['DashboardMetrics'],
+        }),
+    }) 
 });
 
-export const {} = api;
+export const {useGetDashboardMetricsQuery} = api;
